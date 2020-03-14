10696c6eb05b8fe726e9e530c3682872cb918b3d
David Dumolo <ddumolo@hotmail.com>
2020-03-14 15:18:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function iterativeLog(array) {
-  array.forEach(`${index}: ${element}`);
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`)
+  });
 }
 
 function iterate(callback) {
